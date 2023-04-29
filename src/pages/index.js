5cb7f4ff48b209de2c02ad1c0591f0cbf95ec501
faustomontiel <faustomontiel98@outlook.com>
2023-04-29 23:09:29 +0400

    chart

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import Header from '../components/Header'
 import Head from 'next/head'
 import TopCards from '@/components/TopCards'
+import BarChart from '@/components/BarChart'
+import RecentOrders from '@/components/RecentOrders'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +19,10 @@ export default function Home() {
       >
         <Header />
         <TopCards />
+        <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
+          <BarChart />
+          <RecentOrders />
+        </div>
       </main>
     </>
   )
