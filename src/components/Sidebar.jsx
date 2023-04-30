2b94230d12e78829622d91030f380ba134d98506
faustomontiel <faustomontiel98@outlook.com>
2023-05-01 03:08:42 +0400

    Orders - Customers Pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,13 +11,13 @@ export default function Sidebar({ children }) {
         <div className='flex'>
             <div className='fixed w-20 h-screen p-4 bg-white border-r-[1] flex flex-col justify-between'>
                 <div className='flex flex-col items-center'>
-                    <Link rel="stylesheet" href="">
+                    <Link rel="stylesheet" href="/">
                         <div className='bg-purple-800 text-white p-3 rounded-lg inline-block'>
                             <RxSketchLogo size={20} />
                         </div>
                     </Link>
                     <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
-                    <Link rel="stylesheet" href="">
+                    <Link rel="stylesheet" href="/">
                         <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer m-4 p-3 rounded-lg inline-block'>
                             <RxDashboard size={20} />
                         </div>
